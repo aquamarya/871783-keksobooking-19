696a4691d26e1e99541f56933ecf6c8fa75d45e6
Maria Vlasova <vlasova_ya@mail.ru>
2020-02-01 01:03:52 +0400

    makes the task

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 'use strict';
 
 var PRISES = [
-  5000,
-  10000
+  10000,
+  50000
 ];
 var TYPE_OF_HOUSE = [
   'palace',
@@ -43,10 +43,75 @@ var FOTOS = [
   'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
   'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
 ];
-
-var getRandomItem = function (array) {
-  return Math.floor(Math.random() * array.length);
+var pin = {
+  offsetX: 50,
+  offsetY: 60
 };
+var LOCATION_X = 60;
+var LOCATION_Y = 300;
+
+var offsetX = ;
+var offsetY = ;
+
+var getRandomItem = function (length) {
+  return Math.floor(Math.random() * length);
+};
+
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+var createAdverts = function (length) {
+  var adverts = [];
+  for (var i = 0; i < length; i++) {
+    adverts[i] = {
+      author: {
+        avatar: 'img/avatars/user0' + getRandomArbitrary(1, 8) + '.png',
+      },
+      offer: {
+        title: 'Заголовок предложения',
+        address: '600, 350', // "{{location.x}}, {{location.y}}"
+        price: getRandomArbitrary(PRISES),
+        type: getRandomItem(TYPE_OF_HOUSE),
+        rooms: getRandomArbitrary(ROOMS),
+        guests: getRandomArbitrary(GUESTS),
+        checkin: getRandomItem(CHECKIN),
+        checkout: getRandomItem(CHECKOUT),
+        features: getRandomItem(FEATURES),
+        description: 'Описание',
+        photos: getRandomItem(FOTOS)
+      },
+      location: {
+        x: getRandomArbitrary(0, 1200),
+        y: getRandomArbitrary(130, 630)
+      }
+    };
+    return adverts;
+  }
+};
+createAdverts();
 
 var map = document.querySelector('.map');
 map.classList.remove('map--faded');
+
+var mapPins = document.querySelector('.map__pins');
+var mapPinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
+
+var renderMapPin = function (adverts) {
+  var mapPinElement = mapPinTemplate.cloneNode(true);
+  mapPinElement.querySelector('img').src = adverts.author.avatar;
+  mapPinElement.querySelector('img').alt = adverts.offer.title;
+  mapPinElement.style = 'left:' + ' ' + (adverts.location.x + offsetX) + 'px; top:' + ' ' + (adverts.location.y + offsetY) + 'px';
+  return mapPinElement;
+};
+
+renderMapPin();
+
+var renderPins = function (length) {
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < length; i++) {
+    fragment.appendChild(renderMapPin(adverts[i]));
+  }
+};
+
+renderPins();
