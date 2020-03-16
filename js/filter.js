055254c9f5d37c5d7fea60bde964a2028c40b9ee
Maria Vlasova <vlasova_ya@mail.ru>
2020-03-16 16:47:54 +0400

    adds filter

--- js/filter.js
+++ js/filter.js
@@ -6,9 +6,7 @@
   var housingPrice = formFilters.querySelector('#housing-price');
   var housingRooms = formFilters.querySelector('#housing-rooms');
   var housingGuests = formFilters.querySelector('#housing-guests');
-  var housingFeatures = formFilters.querySelector('#housing-features');
-  // var housingFeatures = formFilters.querySelectorAll('.map__checkbox');
-  // var housingFeatures = Array.from(formFilters.querySelectorAll('.map__checkbox'));
+  // var housingFeatures = formFilters.querySelector('#housing-features');
 
   // Значение фильтра по умолчанию
   var defaultFilter = 'any';
@@ -17,15 +15,6 @@
 
   var MIN_PRICE = 10000;
   var MAX_PRICE = 50000;
-
-  // var ADVERT_FEATURES = [
-  //   'wifi',
-  //   'dishwasher',
-  //   'parking',
-  //   'washer',
-  //   'elevator',
-  //   'conditioner'
-  // ];
 
   var onFilterChange = function (event) {
     filteredAdverts = adverts;
@@ -43,9 +32,16 @@
         filteredAdverts = filterByGuests(event.target.value);
         break;
       case event.target.id === ('filter-' + event.target.value):
-        return Array.from(formFilters.querySelectorAll('input:checked')).map(function (advert) {
-          filteredAdverts = filterByFeatures(advert.value);
+        var checked = [];
+        var features = Array.from(document.getElementsByName('features'));
+        features.forEach(function (feature) {
+          if (feature.checked) {
+            checked.push(feature.value);
+          }
         });
+        filteredAdverts = filterByFeatures(checked);
+        break;
+
       default:
         return true;
     }
@@ -66,8 +62,8 @@
   var filterByPrice = function () {
     return filteredAdverts.filter(function (advert) {
       switch (true) {
-        case 'low':
-          return (advert.offer.price < MIN_PRICE);
+        case advert.offer.price < MIN_PRICE:
+          return advert;
         case 'middle':
           return (advert.offer.price <= MAX_PRICE && advert.offer.price >= MIN_PRICE);
         case 'high':
@@ -81,7 +77,7 @@
   var filterByRooms = function (rooms) {
     if (rooms !== defaultFilter) {
       return filteredAdverts.filter(function (advert) {
-        return advert.offer.rooms === rooms;
+        return advert.offer.rooms === parseInt(rooms, 10);
       });
     }
     return false;
@@ -90,36 +86,19 @@
   var filterByGuests = function (guests) {
     if (guests !== defaultFilter) {
       return filteredAdverts.filter(function (advert) {
-        return advert.offer.guests === guests;
+        return advert.offer.guests === parseInt(guests, 10);
       });
     }
     return false;
   };
 
   var filterByFeatures = function (features) {
-    for (var featureIndex = 0; featureIndex < housingFeatures.length; featureIndex++) {
-      filteredAdverts = filteredAdverts.filter(filterByFeatures(housingFeatures[featureIndex]));
-    }
-    if (features !== defaultFilter) {
-      return filteredAdverts.filter(function (advert) {
-        return advert.offer.features === features;
+    return filteredAdverts.filter(function (advert) {
+      return features.every(function (f) {
+        return (advert.offer.features.indexOf(f) > -1);
       });
-    }
-    return false;
+    });
   };
-
-  // var housingFeatures = Array.from(formFilters.querySelectorAll('.map__checkbox'));
-  //
-  // var filterByFeatures = function (advert) {
-  //   var checkedFeatures = housingFeatures.filter(function (feature) {
-  //     return feature.checked;
-  //   });
-  //   var isFeatureChecked = true;
-  //   checkedFeatures.forEach(function (feature) {
-  //     isFeatureChecked = isFeatureChecked && advert.offer.features.includes(feature.getAttribute('value'));
-  //   });
-  //   return isFeatureChecked;
-  // };
 
   var renderAdverts = function (data) {
     adverts = data;
